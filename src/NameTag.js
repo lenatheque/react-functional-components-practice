9e36a4fe5070eda9056ad5b1878996f13530ed89
✬ ۞ ☾ Lorεηα ☽ ۞ ✬ <45369211+lenatheque@users.noreply.github.com>
2021-04-16 13:47:57 -0600

    Passing props props to my functional components

--- src/NameTag.js
+++ src/NameTag.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-var NameTag = () => (
+var NameTag = (props) => (
   <div className="name-tag">
     <h3 className="title">Hello</h3>
     <p className="subtitle"> my name is </p>
-    <h2 className="name"> Lena</h2>
+    <h2 className="name"> {props.name}</h2>
   </div>
 );
 
--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import "./styles.css";
 var App = () => (
   <div className="App">
     <h1>Name Tag Generator</h1>
-    <NameTag />
+    <NameTag name="Jules" />
+    <NameTag name="Marco" />
+    <NameTag name="Lorena" />
   </div>
 );
 
