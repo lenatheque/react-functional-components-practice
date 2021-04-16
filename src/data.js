7dc1e90bf510c0fa9cf51a80f5093e40ac8ea08c
✬ ۞ ☾ Lorεηα ☽ ۞ ✬ <45369211+lenatheque@users.noreply.github.com>
2021-04-16 14:14:40 -0600

    building a data model

---
+++ src/data.js
@@ -0,0 +1,7 @@
+export var names = [
+  { personname: "Jules", id: "0001" },
+  { personname: "Marco", id: "0002" },
+  { personname: "Lorena", id: "0003" },
+  { personname: "Tina", id: "0004" },
+  { personname: "Camila", id: "0005" }
+];
