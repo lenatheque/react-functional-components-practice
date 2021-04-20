34bc9c3a44992353bd2ce23c5d3b75219ce90226
✬ ۞ ☾ Lorεηα ☽ ۞ ✬ <45369211+lenatheque@users.noreply.github.com>
2021-04-20 13:59:52 -0600

    building your own name text input

--- src/NameTag.js
+++ src/NameTag.js
@@ -1,11 +1,17 @@
 import React from "react";
 
-var NameTag = (props) => (
-  <div className="name-tag">
-    <h3 className="title">Hello</h3>
-    <p className="subtitle"> my name is </p>
-    <h2 className="name"> {props.name}</h2>
-  </div>
-);
+var NameTag = (props) => {
+  var deleteByIndex = () => props.removeName(props.index);
+  return (
+    <div className="name-tag">
+      <h3 className="title">HELLO</h3>
+      <p className="subtitle">my name is</p>
+      <h2 className="name">{props.name}</h2>
+      <span onClick={deleteByIndex} className="name-tag__delete">
+        x
+      </span>
+    </div>
+  );
+};
 
 export default NameTag;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,8 @@
-import React, { Component } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
-import NameTag from "./NameTag.js";
+import App from "./App.js";
+
 import "./styles.css";
-
-var renderNameTag = (name) => <NameTag name={name} key={name} />;
-
-class App extends Component {
-  state = { names: ["Erin", "Ann", "Nichole", "Sharon", "Maryn"] };
-  render() {
-    var NameTagElements = this.state.names.map(renderNameTag);
-    return (
-      <div className="App">
-        <h1>Name Tag Generator</h1>
-        {NameTagElements}
-      </div>
-    );
-  }
-}
 
 var rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
